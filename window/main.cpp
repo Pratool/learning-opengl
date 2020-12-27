28716dd2cbc295a22a4b52c0495ec6734234600f
Pratool Gadtaula <5598110+Pratool@users.noreply.github.com>
2020-12-27 15:16:10 -0500

    Compile vertex shader

--- window/main.cpp
+++ window/main.cpp
@@ -4,6 +4,18 @@
 #include <iostream>
 #include <memory>
 
+float vertices[] = {
+    -0.5F,  -0.5F,  0.0F,
+    0.5F,   -0.5F,  0.0F,
+    0.0F,   0.5F,   0.0F
+};
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0F);\n"
+    "}\0";
+
 void framebufferSizeCallback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -45,6 +57,28 @@ int main()
     }
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (not static_cast<bool>(success))
+    {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cerr << "Failed to compile vertex shader." << std::endl
+                  << infoLog << std::endl;
+    }
+
 
     while (not glfwWindowShouldClose(window))
     {
