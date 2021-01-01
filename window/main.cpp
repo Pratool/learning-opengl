2ddc27502055f0c6573c4edabd2f0202cc61a7b3
Pratool Gadtaula <5598110+Pratool@users.noreply.github.com>
2021-01-01 18:40:14 -0500

    Draw a rectangle with an element array buffer

--- window/main.cpp
+++ window/main.cpp
@@ -5,9 +5,14 @@
 #include <memory>
 
 float vertices[] = {
+     0.5F,   0.5F,  0.0F,
+     0.5F,  -0.5F,  0.0F,
     -0.5F,  -0.5F,  0.0F,
-    0.5F,   -0.5F,  0.0F,
-    0.0F,   0.5F,   0.0F
+    -0.5F,   0.5F,  0.0F
+};
+unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3
 };
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
@@ -110,15 +115,20 @@ int main()
     glDeleteShader(fragmentShader);
 
 
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
     unsigned int VBO;
     glGenBuffers(1, &VBO);
 
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
@@ -133,7 +143,7 @@ int main()
         glClearColor(0.2F, 0.3F, 0.3F, 1.0F);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
